67838c8bb8f2205833d2ecf3d0af99dc47fb9a78
Mathi Manavalan <mathimano@hotmail.com>
2018-03-30 11:31:44 -0700

    one dropdown displaying

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
+window.onload = function() {
+		document.getElementById("dropdown").onclick = myFunction;
+};
 
 
 
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
 
-(function() {
-	
-	"use strict";
-	
-	window.onload = function() {
-		
-	};
-	
-	
-})();
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
