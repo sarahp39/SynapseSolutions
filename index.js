4068dea1403ab3325d8395595670fe0cc3eeb247
Mathi Manavalan <mathimano@hotmail.com>
2018-04-01 16:23:03 -0700

    added bottom banner and css'd both banners; established arrangement and some css of body div; aded and css'd bottom half text and button

--- index.js
+++ index.js
@@ -11,8 +11,6 @@ window.onload = function() {
 		
 };
 
-
-
 function myFunction() {
 	
 	var btnArr = document.getElementsByClassName("dropbtn");
@@ -20,40 +18,12 @@ function myFunction() {
 	for (j = 0; j < btnArr.length; j++) {
 		
 		if (btnArr[j].id != this.id) {
+			// slice to get rid of btn of the id
 			document.getElementById(btnArr[j].id.slice(0,-3).concat("Dropdown")).classList.remove("show");
-			
 		}
 	}
 	
 	document.getElementById(this.id.slice(0,-3).concat("Dropdown")).classList.toggle("show");
-	
-	/*
-	//if button's id is this, show corresponding dropdown
-	if (this.id == "databtn") {
-		
-		//console.log(btnArr[1].id);
-		//console.log(btnArr[2].id);
-		
-		//document.getElementById(btnArr[1].id).classList.remove("show");
-		//document.getElementById(btnArr[2].id).classList.remove("show");
-		
-		document.getElementById("dataDropdown").classList.toggle("show"); //toggle so if databtn clicked second time, dropdown disappears
-		
-	} else if (this.id == "resourcesbtn") {		
-		//document.getElementById("dataDropdown").classList.remove("show");
-		//document.getElementById("aboutDropdown").classList.remove("show");
-		
-		document.getElementById("resourcesDropdown").classList.toggle("show");
-		
-	} else if (this.id == "aboutbtn") {		
-		//document.getElementById("dataDropdown").classList.remove("show");
-		//document.getElementById("resourcesDropdown").classList.remove("show");
-		
-		document.getElementById("aboutDropdown").classList.toggle("show");
-		
-	}
-	
-	*/
     //document.getElementById("resourcesDropdown").classList.toggle("show"); // id was myDropdown
 }
 
@@ -61,8 +31,6 @@ function myFunction() {
 // Close the dropdown menu if the user clicks outside of it
 window.onclick = function(event) {
   if (!event.target.matches('.dropbtn')) { //if click anywhere other than a dropdown button, 'hide' all dropdowns
-  
-  //then check the btn id 
 
     var dropdowns = document.getElementsByClassName("dropdown-content");
     var i;
